e2aeac31ea232ae5be30850cc86e58f0a8e8622b
syrvyss <lybkern@gmail.com>
2023-11-10 13:30:44 +0100

    Added basic administration sites

---
+++ WebApplication1/Pages/ItemAdmin.cshtml.cs
@@ -0,0 +1,58 @@
+using System.ComponentModel.DataAnnotations;
+using Data.Entities;
+using Data.Repositories;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+
+namespace PokeChopShop.Pages;
+
+public class ItemAdmin : PageModel
+{
+    private IPokemonRepository _pokemonRepository;
+    private IPokemonStatsRepository _pokemonStatsRepository;
+
+    public Pokemon Pokemon { get; set; }
+
+    [BindProperty(SupportsGet = true)] public int Id { get; set; }
+
+    [BindProperty] public string Name { get; set; }
+    [BindProperty] public int Experience { get; set; }
+    [BindProperty] public int Height { get; set; }
+    [BindProperty] public int Weight { get; set; }
+    [BindProperty] public string Description { get; set; }
+
+    public ItemAdmin(IPokemonRepository pokemonRepository, IPokemonStatsRepository pokemonStatsRepository)
+    {
+        _pokemonRepository = pokemonRepository;
+        _pokemonStatsRepository = pokemonStatsRepository;
+    }
+
+    public void OnGet(int id)
+    {
+        ViewData["ItemId"] = id;
+        Id = id;
+
+        Pokemon = _pokemonRepository.GetFullById(id);
+    }
+
+    public async Task<IActionResult> OnPost()
+    {
+        if (!ModelState.IsValid) return Page();
+
+        var pokemon = _pokemonRepository.GetFullById(Id);
+        // var pokemonStats = _pokemonStatsRepository.GetById(Id);
+
+        pokemon.Name = Name;
+        pokemon.PokemonStats.Experience = Experience;
+        pokemon.PokemonStats.Height = Height;
+        pokemon.PokemonStats.Weight = Weight;
+        pokemon.PokemonStats.Description = Description;
+
+        _pokemonRepository.Update(pokemon);
+        // _pokemonStatsRepository.Update(pokemonStats);
+
+        return RedirectToPage("Admin");
+    }
+}
---
+++ WebApplication1/Pages/ItemAdminCreate.cshtml.cs
@@ -0,0 +1,61 @@
+using System.ComponentModel.DataAnnotations;
+using Data.Entities;
+using Data.Repositories;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+
+namespace PokeChopShop.Pages;
+
+public class ItemAdminCreate : PageModel
+{
+    private IPokemonRepository _pokemonRepository;
+    private IPokemonStatsRepository _pokemonStatsRepository;
+
+    [BindProperty] public string Name { get; set; }
+    [BindProperty] public string Sprite { get; set; }
+    [BindProperty] public int Experience { get; set; }
+    [BindProperty] public int Height { get; set; }
+    [BindProperty] public int Weight { get; set; }
+    [BindProperty] public string Description { get; set; }
+
+    public ItemAdminCreate(IPokemonRepository pokemonRepository, IPokemonStatsRepository pokemonStatsRepository)
+    {
+        _pokemonRepository = pokemonRepository;
+        _pokemonStatsRepository = pokemonStatsRepository;
+    }
+
+    public void OnGet()
+    {
+    }
+
+    public async Task<IActionResult> OnPost()
+    {
+        if (!ModelState.IsValid) return Page();
+
+        var maxId = _pokemonRepository.GetAll().MaxBy(x => x.Id).Id;
+
+
+        var pokemon = new Pokemon()
+        {
+            Id = maxId + 1,
+            Name = Name,
+            Sprite = Sprite,
+            Quantity = 0,
+            Orders = new List<Order>(),
+            PokemonStats = new PokemonStats()
+            {
+                Id = maxId + 1,
+                Experience = Experience,
+                Height = Height,
+                Weight = Weight,
+                Description = Description
+            }
+        };
+
+        _pokemonRepository.Add(pokemon);
+
+        return RedirectToPage("Admin");
+    }
+}

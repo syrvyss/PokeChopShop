cbc8003f7ec5ebd1b02a1e749f9ca70783b05486
syrvyss <lybkern@gmail.com>
2023-11-10 13:23:26 +0100

    Added admin site for Pokemon, Orders

---
+++ WebApplication1/Pages/Admin.cshtml.cs
@@ -0,0 +1,37 @@
+using Data.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PokeChopShop.Pages;
+
+public class Admin : PageModel
+{
+    private IPokemonRepository _pokemonRepository;
+    private IOrderRepository _orderRepository;
+
+    public Admin(IPokemonRepository pokemonRepository, IOrderRepository orderRepository)
+    {
+        _pokemonRepository = pokemonRepository;
+        _orderRepository = orderRepository;
+    }
+
+    public void OnGet()
+    {
+    }
+
+    public IActionResult OnPostDeletePokemon(int id)
+    {
+        var getPokemon = _pokemonRepository.GetById(id);
+
+        _pokemonRepository.Delete(getPokemon);
+        return Page();
+    }
+
+    public IActionResult OnPostDeleteOrder(int id)
+    {
+        var getOrder = _orderRepository.GetById(id);
+
+        _orderRepository.Delete(getOrder);
+        return Page();
+    }
+}

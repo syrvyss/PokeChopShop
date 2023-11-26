a41abdc2edd536a97c7a15b744fccef305a30e37
syrvyss <69795518+syrvyss@users.noreply.github.com>
2023-11-26 18:07:39 +0100

    Admin pages (#36)
    * Add basic admin pages
    
    * Add HttpHelper for easier http handling

--- WebAppBlazor/Program.cs
+++ WebAppBlazor/Program.cs
@@ -1,6 +1,8 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WebAppBlazor;
+using WebAppBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,6 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddTransient<HttpHelper>();
 builder.Services.AddTransient(x => new HttpClient { BaseAddress = new Uri("https://localhost:7123") });
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
